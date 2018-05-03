24712f71f16619e33d8d6f1e9501cf9e4e1a98ac
owen <770104798@qq.com>
2018-05-03 23:53:49 +0800

    Implement DB design with migration.

---
+++ final-design/database/migrations/2018_05_03_000000_create_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('num');
+            $table->string('name');
+            $table->string('telephone_num');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->integer('school_id')->nullable();
+            $table->string('role')->default('student');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
---
+++ final-design/resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" />
+    @yield('css')
+    <title>@yield('title')</title>
+</head>
+<body>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <script src="{{asset('js/vue.js')}}"></script>
+    <script src="{{asset('js/jquery.js')}}"></script>
+    @yield('js')
+</body>
+</html>
